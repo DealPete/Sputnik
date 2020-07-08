f4f7338e2d5dffa75d54721ad4f2183c418b46b2
Peter Deal <accounts@peterdeal.ca>
2020-07-08 20:21:54 -0230

    Initial Commit

---
+++ Sputnik/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Sputnik
+//
+//  Created by Peter Deal on 2020-07-08.
+//  Copyright © 2020 Peter Deal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
